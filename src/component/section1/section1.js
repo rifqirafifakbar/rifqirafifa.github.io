c1723784c6acb9f9948b78fda0722bc576b301e6
Rifqi Rafif Akbar <rifqi.akbar@xtremax.com>
2022-01-03 20:46:22 +0700

    update section 1

--- src/component/section1/section1.js
+++ src/component/section1/section1.js
@@ -1,5 +1,5 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 
@@ -14,24 +14,17 @@ export const Section1 = ()=> {
                 <div className="container px-3">
                     <div className="row justify-content-center pt-5">
                         <div className="col-lg-6 text-center">
-                            <div className='profile'>
-                                {/* <Image
-                                    src={profilePic}
-                                    alt="Picture of the author"
-                                    // width={500} automatically provided
-                                    // height={500} automatically provided
-                                    // blurDataURL="data:..." automatically provided
-                                    // placeholder="blur" // Optional blur-up while loading
-                                /> */}
-                            </div>
+                            <div className='profile'/>
                             <div className="mt-5">
                                 <p className="lead text-uppercase mb-1">Hello!</p>
                                 <h1 className="intro-title marker" data-aos="fade-left" data-aos-delay="50">I’m Rifqi Rafif Akbar</h1>
                                 <p className="lead fw-normal mt-3" data-aos="fade-up" data-aos-delay="100">Frontend Developer</p>
+                                <p className=" mt-3" data-aos="fade-up" data-aos-delay="100">I am passionate about Front-end Engineering. I'm familiar with React, Next.js and JQuery, I used to working together using Git and Scrum working method system. I have advanced in Photoshop / Corel / AI, also I'm familiar with e2e testing used cypress tools and I'd implement Accessibility, SEO, Best Practice, and Progressive Web Apps in the development website.
+                                    I took the Hackaton event and got The Top 6 Of The Smart City Category at The National level at Telkom Indonesia..</p>
                                 <div className="social-nav" data-aos="fade-up" data-aos-delay="200">
                                     <nav role="navigation">
                                         <ul className="nav justify-content-center">
-                                        <li className="nav-item"><a className="nav-link" href="https://www.linkedin.com/in/rifqirafifa/" title="LinkedIn"><FontAwesomeIcon icon={faTwitter}/><span className="menu-title sr-only">LinkedIn</span></a></li>
+                                        <li className="nav-item"><a className="nav-link" href="https://www.linkedin.com/in/rifqirafifa/" title="LinkedIn"><FontAwesomeIcon size="lg" icon={faLinkedinIn}/><span className="menu-title sr-only">LinkedIn</span></a></li>
                                         </ul>
                                     </nav>
                                 </div>
